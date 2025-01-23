8ca4f356324baaff8ce26d4f7f0ce6524095e08a
RayhanSohel <arayhansohel@gmail.com>
2025-01-24 00:47:22 +0600

    add navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+
 
 
 const geistSans = Geist({
@@ -20,9 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="text-sm">
+        <Navbar/>
         {children}
       </body>
     </html>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleAuth = () => {
+    setIsAuthenticated(!isAuthenticated);
+  };
+
+  return (
+    <>
+      <nav className="p-4 bg-base-200">
+        <div className="flex justify-between items-center container mx-auto">
+          <div>
+            <Link href="/">
+              <Image
+                src="/tech-box-logo-text.png"
+                alt="TechBox Logo"
+                width={150}
+                height={50}
+                priority
+              />
+            </Link>
+          </div>
+          <div className="space-x-4">
+            <Link href="/">
+              <button className="hover:text-accent transition">
+                Home
+              </button>
+            </Link>
+            <Link href="/profile">
+              <button className="hover:text-accent transition">
+                Profile
+              </button>
+            </Link>
+            <button
+              onClick={handleAuth}
+              className="btn btn-sm btn-accent px-4 py-2 rounded-md"
+            >
+              {isAuthenticated ? "Logout" : "Login"}
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
