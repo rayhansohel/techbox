aeb90678fcbcea46d4652403c13af0e3bbeb8d11
RayhanSohel <arayhansohel@gmail.com>
2025-01-23 20:56:57 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import { cn } from "./../lib/utils";
+
+const page = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      Hello Techbox
+    </div>
+  );
+};
+
+export default page;
